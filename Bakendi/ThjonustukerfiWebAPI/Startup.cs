c117d8c5cf4c4908433943be3283e3f91ef66030
ViktorSveins <viktorsveins@gmail.com>
2020-02-19 12:59:38 +0000

    docker compose and fallback api connectionstring

--- Bakendi/ThjonustukerfiWebAPI/Startup.cs
+++ Bakendi/ThjonustukerfiWebAPI/Startup.cs
@@ -28,9 +28,17 @@ namespace ThjonustukerfiWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-						services.AddDbContext<DataContext>(options => {
-							options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
-						});
+            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            services.AddDbContext<DataContext>(options => {
+                if (connectionString != null)
+                {
+                    options.UseNpgsql(connectionString);
+                }
+                else
+                {
+                    options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
