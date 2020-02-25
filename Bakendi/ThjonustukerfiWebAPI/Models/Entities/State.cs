080189edcfea13e9454b91d1c796dbbae6798f21
DStrik <dannimb@gmail.com>
2020-02-25 14:12:05 +0000

    added state model

---
+++ Bakendi/ThjonustukerfiWebAPI/Models/Entities/State.cs
@@ -0,0 +1,9 @@
+namespace ThjonustukerfiWebAPI.Models.Entities
+{
+    public class State
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string JSON { get; set; }
+    }
+}
