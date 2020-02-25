6bb07eb123ab078648bf20e9b79b77cd29340532
DStrik <dannimb@gmail.com>
2020-02-25 14:11:37 +0000

    added service model

---
+++ Bakendi/ThjonustukerfiWebAPI/Models/Entities/Service.cs
@@ -0,0 +1,9 @@
+namespace ThjonustukerfiWebAPI.Models.Entities
+{
+    public class Service
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string JSON { get; set; }
+    }
+}
