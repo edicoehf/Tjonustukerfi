7e0f865a611a081e4092d9a0fcc4fe48b056857e
ViktorSveins <viktorsveins@gmail.com>
2020-02-28 10:05:56 +0000

    input component

---
+++ Vefur/thjonustukerfi-react-app/src/components/Input/Input.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = props => {
+    const { value, onInput, type, errorMessage, label, name, htmlId } = props;
+    return (
+   	   <div className="form-group">
+		{ label ? <label htmlFor={ htmlId } className="control-label">{ label }</label> : <></> }
+			<input
+				type={ type }
+				value={ value }
+				onChange={ onInput }
+				name={ name }
+				id={ htmlId }
+				className="form-control" />
+			<span className="error">{ errorMessage }</span>
+      	</div>
+    );
+};
+
+Input.propTypes = {
+	value: PropTypes.string.isRequired,
+	htmlId: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	onInput: PropTypes.func.isRequired,
+	type: PropTypes.oneOf([ 'text' ]),
+	errorMessage: PropTypes.string,
+	label: PropTypes.string
+};
+
+export default Input;
