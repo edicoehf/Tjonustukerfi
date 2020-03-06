e1c0e41f978dc173e366208382b76c66528c8107
DStrik <dannimb@gmail.com>
2020-03-06 11:16:24 +0000

    added view model for input form that changes the state on input

---
+++ Handtolva/HandtolvuApp/HandtolvuApp/HandtolvuApp/ViewModels/InputViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace HandtolvuApp.ViewModels
+{
+    class InputViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string inputVariable;
+
+        public string InputVariable
+        {
+            get => inputVariable;
+
+            set
+            {
+                inputVariable = value;
+
+                var args = new PropertyChangedEventArgs(nameof(InputVariable));
+
+                PropertyChanged?.Invoke(this, args);
+
+                System.Diagnostics.Debug.WriteLine("Input: " + inputVariable);
+            }
+        }
+    }
+}
