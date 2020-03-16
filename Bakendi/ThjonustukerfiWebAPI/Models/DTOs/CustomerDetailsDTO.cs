e2850c82768a28f744d613536439f3c9811e8700
ViktorSveins <viktorsveins@gmail.com>
2020-03-16 16:14:10 +0000

    created CustomerDetailsDTO

---
+++ Bakendi/ThjonustukerfiWebAPI/Models/DTOs/CustomerDetailsDTO.cs
@@ -0,0 +1,14 @@
+namespace ThjonustukerfiWebAPI.Models.DTOs
+{
+    public class CustomerDetailsDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string SSN { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string JSON { get; set; }
+    }
+}
