23d224d6fad9e1ec62973b29bc5e2c8d6a0c430a
Danni <dannimb@gmail.com>
2020-03-16 13:27:38 +0000

    got all tests to work, mocking database correctly now

--- Bakendi/ThjonustukerfiTests/Tests/CustomerRepoTests.cs
+++ Bakendi/ThjonustukerfiTests/Tests/CustomerRepoTests.cs
@@ -1,20 +1,13 @@
-using System.Net;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 using ThjonustukerfiWebAPI.Repositories.Implementations;
 using ThjonustukerfiWebAPI.Models.DTOs;
 using ThjonustukerfiWebAPI.Models.Entities;
-using ThjonustukerfiWebAPI.Repositories.Interfaces;
 using ThjonustukerfiWebAPI.Models;
 using FizzWare.NBuilder;
-using NSubstitute;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
 using ThjonustukerfiWebAPI.Mappings;
-using System.Diagnostics;
 using ThjonustukerfiWebAPI.Models.InputModels;
 
 namespace ThjonustukerfiTests.Tests
@@ -23,8 +16,8 @@ namespace ThjonustukerfiTests.Tests
     public class CustomerRepoTests
     {
         // private IDataContext _dbContextMock;
-        private ICustomerRepo _customerRepo;
-        private Mock<DataContext> _mockContext;
+        private Mapper _mapper;
+        private DbContextOptions<DataContext> _options;
 
         [TestInitialize]
         public void Initialize()
@@ -33,45 +26,46 @@ namespace ThjonustukerfiTests.Tests
             // Add automapper to use in the repo constructor
             var myProfile = new MappingProfile();       // Create a new profile like the one we implemented
             var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));  // Setup a configuration with our profile
-            var mapper = new Mapper(configuration);     // Create a new mapper with our profile
+            _mapper = new Mapper(configuration);     // Create a new mapper with our profile
 
-            // Mock dbcontext and data
-            // Build a list of size 20, make it queryable for the database mock
-            var customers = Builder<Customer>.CreateListOfSize(20)
-                .TheFirst(1).With(x => x.Name = "Viggi Siggi").With(x => x.Id = 10).With(x => x.Email = "VS@vigsig.is")
-                .Build();
-            var customerQueryable = customers.AsQueryable();
+            _options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: "Customer")
+            .EnableSensitiveDataLogging()
+            .Options;
+        }
 
-            // Setup the Mock for Customer DbSet
-            var mockSet = new Mock<DbSet<Customer>>();
-            mockSet.As<IQueryable<Customer>>().Setup(m => m.Provider).Returns(customerQueryable.Provider);
-            mockSet.As<IQueryable<Customer>>().Setup(m => m.Expression).Returns(customerQueryable.Expression);
-            mockSet.As<IQueryable<Customer>>().Setup(m => m.ElementType).Returns(customerQueryable.ElementType);
-            mockSet.As<IQueryable<Customer>>().Setup(m => m.GetEnumerator()).Returns(customerQueryable.GetEnumerator());
-            mockSet.Setup(x => x.Add(It.IsAny<Customer>())).Callback<Customer>((Customer c) => customers.Add(c));
-
-            // _mockContext.Setup(d => d.Add(It.IsAny<Customer>())).Callback<Customer>((s) => mockSet.Object.Add(s));
-            // Create and setup a mock of our DataContext class that is injected to the customer repo constructor
-            _mockContext = new Mock<DataContext>();
-            _mockContext.Setup(c => c.Customer).Returns(mockSet.Object);
-            // _mockContext.Setup(x => x.Customer.Add(It.IsAny<Customer>())).Callback(customers.);
-            
-            // Create a new customer repository and inject the mock for our database and our mapper
-            _customerRepo = new CustomerRepo(_mockContext.Object, mapper);
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            _mapper = null;
+            _options = null;
         }
 
         [TestMethod]
-        public void GetCustomer_should_return_customer_with_id_10()
+        public void GetCustomer_should_return_customer_with_id_100()
         {
             // Arrange
-            
-            // Act
-            var result = _customerRepo.GetCustomerById(10);
+            using (var mockContext = new DataContext(_options))
+            {
+                //! Add only ONCE! Unless appending changes
+                // Build a list of size 20, make it queryable for the database mock
+                var customers = Builder<Customer>.CreateListOfSize(20)
+                .TheFirst(1).With(x => x.Name = "Viggi Siggi").With(x => x.Id = 100).With(x => x.Email = "VS@vigsig.is")
+                .Build();
 
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(result.Name, "Viggi Siggi");
-            Assert.AreEqual(result.Id, 10);
+                mockContext.Customer.AddRange(customers);
+                mockContext.SaveChanges();
+
+                var customerRepo = new CustomerRepo(mockContext, _mapper);
+
+                // Act
+                var result = customerRepo.GetCustomerById(100);
+
+                // Assert
+                Assert.IsNotNull(result);
+                Assert.AreEqual(result.Name, "Viggi Siggi");
+                Assert.AreEqual(result.Id, 100);
+            }
         }
 
         [TestMethod]
@@ -83,17 +77,23 @@ namespace ThjonustukerfiTests.Tests
                 Name = "Kalli Valli",
                 Email = "KV@vigsig.is"
             };
-            var dbSize = _mockContext.Object.Customer.ToList().Count;
 
-            // // Act
-            // var result = _customerRepo.CreateCustomer(inp);
+            using (var mockContext = new DataContext(_options))
+            {
+                var customerRepo = new CustomerRepo(mockContext, _mapper);
 
-            // // Assert
-            // Assert.IsNotNull(result);
-            // Assert.IsInstanceOfType(result, typeof(CustomerDTO));
-            // Assert.AreEqual(_mockContext.Object.Customer.ToList().Count, dbSize + 1);
-            // Assert.AreEqual(result.Name, inp.Name);
-            // Assert.IsInstanceOfType(result.Id, typeof(long));
+                var dbSize = mockContext.Customer.Count();
+
+                // Act
+                var result = customerRepo.CreateCustomer(inp);
+
+                // Assert
+                Assert.IsNotNull(result);
+                Assert.IsInstanceOfType(result, typeof(CustomerDTO));
+                Assert.AreEqual(mockContext.Customer.Count(), dbSize + 1);
+                Assert.AreEqual(result.Name, inp.Name);
+                Assert.IsInstanceOfType(result.Id, typeof(long));
+            };
         }
     }
 }
