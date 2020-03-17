6c42c1f5e612afa0951b06d13e9316bd8746d47e
ViktorSveins <viktorsveins@gmail.com>
2020-03-17 11:45:25 +0000

    moved display customer title to customerview

--- Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerDetails/CustomerDetails.js
+++ Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerDetails/CustomerDetails.js
@@ -6,17 +6,17 @@ import './CustomerDetails.css';
 
 const CustomerDetails = ({ id }) => {
   const { customer, error } = useCustomerService(id);
+
   return (
     <div className='customer-details'>
-      <h1>Upplýsingar um viðskiptavin</h1>
       {!error ? ( 
         <table className="customer-properties">
           <CustomerProperty title='Nafn' value={customer.name} />
           <CustomerProperty title='Kennitala' value={customer.ssn} />
           <CustomerProperty title='Sími' value={customer.telephone} />
           <CustomerProperty title='Netfang' value={customer.email} />
-          <CustomerProperty title='Póstnúmer' value={customer.postalCode} />
           <CustomerProperty title='Heimilisfang' value={customer.address} />
+          <CustomerProperty title='Póstnúmer' value={customer.postalCode} />
         </table>
       ) : (
         <p className="error">{error}</p>
--- Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerView/CustomerView.js
+++ Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerView/CustomerView.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import CustomerDetails from '../CustomerDetails/CustomerDetails';
+import './CustomerView.css'
 
 const CustomerView = ({ match }) => {
   const id = match.params.id;
 
   return (
-    <CustomerDetails id={id} />
+    <div className="customer-view">
+      <h1>Upplýsingar um viðskiptavin</h1>
+      <CustomerDetails id={id} />
+    </div>
   )
 };
 
