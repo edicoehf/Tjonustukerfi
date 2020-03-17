dc12be9ff9d4fd7959896c6e8ff77efe4f888b9a
Danni <dannimb@gmail.com>
2020-03-17 12:55:17 +0000

    added automatic migrations at startup

---
+++ Bakendi/ThjonustukerfiWebAPI/Extensions/Webhostextension.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ThjonustukerfiWebAPI.Models;
+
+namespace ThjonustukerfiWebAPI.Extensions
+{
+    public static class WebhostExtension
+    {
+        public static IHost MigrateDatabase<T>(this IHost webHost) where T:DbContext
+        {
+            var serviceScopeFactory = (IServiceScopeFactory)webHost.Services.GetService(typeof(IServiceScopeFactory));
+
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetRequiredService<T>();
+
+                dbContext.Database.Migrate();
+            }
+
+            return webHost;
+        }
+    }
+}
--- Bakendi/ThjonustukerfiWebAPI/Program.cs
+++ Bakendi/ThjonustukerfiWebAPI/Program.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ThjonustukerfiWebAPI.Models;
+using ThjonustukerfiWebAPI.Extensions;
+using System.Diagnostics;
 
 namespace ThjonustukerfiWebAPI
 {
@@ -13,7 +18,10 @@ namespace ThjonustukerfiWebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .MigrateDatabase<DataContext>()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +30,6 @@ namespace ThjonustukerfiWebAPI
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
