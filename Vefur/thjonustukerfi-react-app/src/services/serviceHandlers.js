7af3e3e762d55a27e132c3b35355985c05525325
ViktorSveins <viktorsveins@gmail.com>
2020-03-17 14:52:27 +0000

    servicehandler prettier

--- Vefur/thjonustukerfi-react-app/src/services/serviceHandlers.js
+++ Vefur/thjonustukerfi-react-app/src/services/serviceHandlers.js
@@ -1,10 +1,10 @@
 const handleErrors = resp => {
-  if (!resp.ok) { 
-    throw Error(resp.statusText); 
-  }
-  return resp.json();
-}
+    if (!resp.ok) {
+        throw Error(resp.statusText);
+    }
+    return resp.json();
+};
 
 module.exports = {
-  handleErrors
+    handleErrors
 };
