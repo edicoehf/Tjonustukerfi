89940c5cb19d34268fbc24887afe1cc19215239a
ViktorSveins <viktorsveins@gmail.com>
2020-03-17 12:02:16 +0000

    added tbody to customerdetails table

--- Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerDetails/CustomerDetails.js
+++ Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerDetails/CustomerDetails.js
@@ -11,12 +11,14 @@ const CustomerDetails = ({ id }) => {
     <div className='customer-details'>
       {!error ? ( 
         <table className="customer-properties">
-          <CustomerProperty title='Nafn' value={customer.name} />
-          <CustomerProperty title='Kennitala' value={customer.ssn} />
-          <CustomerProperty title='Sími' value={customer.telephone} />
-          <CustomerProperty title='Netfang' value={customer.email} />
-          <CustomerProperty title='Heimilisfang' value={customer.address} />
-          <CustomerProperty title='Póstnúmer' value={customer.postalCode} />
+          <tbody>
+            <CustomerProperty title='Nafn' value={customer.name} />
+            <CustomerProperty title='Kennitala' value={customer.ssn} />
+            <CustomerProperty title='Sími' value={customer.telephone} />
+            <CustomerProperty title='Netfang' value={customer.email} />
+            <CustomerProperty title='Heimilisfang' value={customer.address} />
+            <CustomerProperty title='Póstnúmer' value={customer.postalCode} />
+          </tbody>
         </table>
       ) : (
         <p className="error">Villa kom upp: Gat ekki sótt viðskiptavin</p>
