72aca093ef73a0157b945dc8103f560623a5a305
ViktorSveins <viktorsveins@gmail.com>
2020-03-23 14:03:57 +0000

    rm uneccesary package files

--- Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerDetails/CustomerDetails.js
+++ Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerDetails/CustomerDetails.js
@@ -1,34 +1,60 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import useCustomerService from '../../../hooks/useCustomerService';
-import CustomerProperty from '../CustomerProperty/CustomerProperty';
-import './CustomerDetails.css';
+import useCustomerService from "../../../hooks/useCustomerService";
+import CustomerProperty from "../CustomerProperty/CustomerProperty";
+import "./CustomerDetails.css";
 
 const CustomerDetails = ({ id }) => {
-  const { customer, error } = useCustomerService(id);
-
-  return (
-    <div className='customer-details'>
-      {!error ? ( 
-        <table className="customer-properties">
-          <tbody>
-            <CustomerProperty title='Nafn' name="name" value={customer.name} />
-            <CustomerProperty title='Kennitala' name="ssn" value={customer.ssn} />
-            <CustomerProperty title='Sími' name="telephone" value={customer.telephone} />
-            <CustomerProperty title='Netfang' name="email" value={customer.email} />
-            <CustomerProperty title='Heimilisfang' name="address" value={customer.address} />
-            <CustomerProperty title='Póstnúmer' name="postalcode" value={customer.postalCode} />
-          </tbody>
-        </table>
-      ) : (
-        <p className="error">Villa kom upp: Gat ekki sótt viðskiptavin</p>
-      )}
-    </div>
-  )
+    const { customer, error } = useCustomerService(id);
+    console.log(customer);
+    return (
+        <div className="customer-details">
+            {!error ? (
+                <table className="customer-properties">
+                    <tbody>
+                        <CustomerProperty
+                            title="Nafn"
+                            name="name"
+                            value={customer.name}
+                        />
+                        <CustomerProperty
+                            title="Kennitala"
+                            name="ssn"
+                            value={customer.ssn}
+                        />
+                        <CustomerProperty
+                            title="Sími"
+                            name="telephone"
+                            value={customer.telephone}
+                        />
+                        <CustomerProperty
+                            title="Netfang"
+                            name="email"
+                            value={customer.email}
+                        />
+                        <CustomerProperty
+                            title="Heimilisfang"
+                            name="address"
+                            value={customer.address}
+                        />
+                        <CustomerProperty
+                            title="Póstnúmer"
+                            name="postalcode"
+                            value={customer.postalCode}
+                        />
+                    </tbody>
+                </table>
+            ) : (
+                <p className="error">
+                    Villa kom upp: Gat ekki sótt viðskiptavin
+                </p>
+            )}
+        </div>
+    );
 };
 
 CustomerDetails.propTypes = {
-  id: PropTypes.string.isRequired
-}
+    id: PropTypes.string.isRequired
+};
 
 export default CustomerDetails;
