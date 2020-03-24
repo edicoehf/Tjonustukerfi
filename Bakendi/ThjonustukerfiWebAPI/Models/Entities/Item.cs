c4ca8aa0df564c1aaebfefaeb4ecc9931ed83f46
Danni <dannimb@gmail.com>
2020-03-24 11:48:16 +0000

    added overrides to Item entity to simplify tests when using equals

--- Bakendi/ThjonustukerfiWebAPI/Models/Entities/Item.cs
+++ Bakendi/ThjonustukerfiWebAPI/Models/Entities/Item.cs
@@ -17,5 +17,42 @@ namespace ThjonustukerfiWebAPI.Models.Entities
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
         public DateTime? DateCompleted { get; set; }
+
+        //*     Overrides     *//
+        public static bool operator ==(Item i1, Item i2)
+        {
+            if(object.ReferenceEquals(i1, i2))
+            {
+                return true;
+            }
+            if(object.ReferenceEquals(i1, null) || object.ReferenceEquals(i2, null))
+            {
+                return false;
+            }
+
+            return i1.Id == i2.Id && i1.Type == i2.Type && i1.StateId == i2.StateId &&
+                        i1.ServiceId == i2.ServiceId && i1.Barcode == i2.Barcode && i1.JSON == i2.JSON &&
+                        i1.DateCreated == i2.DateCreated && i1.DateModified == i2.DateModified && i1.DateCompleted == i2.DateCompleted;
+        }
+
+        public static bool operator !=(Item i1, Item i2)
+        {
+            return !(i1 == i2);
+        }
+
+        public override int GetHashCode()
+        {
+            return (int) Id;
+        }
+
+        public bool Equals(Item other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Item);
+        }
     }
 }
