da6b04655db0d36fa6282be869d10b866908f20a
Danni <dannimb@gmail.com>
2020-03-31 14:14:50 +0000

    added getServices

--- Bakendi/ThjonustukerfiWebAPI/Controllers/InfoController.cs
+++ Bakendi/ThjonustukerfiWebAPI/Controllers/InfoController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ThjonustukerfiWebAPI.Services.Interfaces;
 
@@ -13,6 +14,17 @@ namespace ThjonustukerfiWebAPI.Controllers
         public InfoController(IInfoService inforservice)
         {
             _infoService = inforservice;
+        }
+
+        /// <summary>Gets all available services</summary>
+        /// <returns>OK 200 status</returns>
+        /// <response code="200">Returns a list of services, empty list if there are none</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("")]
+        [HttpGet]
+        public IActionResult GetServices()
+        {
+            return Ok(_infoService.GetServices());
         }
     }
 }
