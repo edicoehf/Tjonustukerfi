3e05b2c852992fb914464eb244a20b5d32828da3
ViktorSveins <viktorsveins@gmail.com>
2020-04-02 14:56:07 +0000

    added hook for getServices

---
+++ Vefur/thjonustukerfi-react-app/src/hooks/useGetServices.js
@@ -0,0 +1,20 @@
+import React from "react";
+import serviceService from "../services/serviceService";
+
+const useGetServices = () => {
+    const [services, setServices] = React.useState({});
+    const [error, setError] = React.useState(null);
+
+    React.useEffect(() => {
+        serviceService
+            .getServices()
+            .then(services => {
+                setOrder(services);
+                setError(null);
+            })
+            .catch(error => setError(error));
+    }, [id]);
+    return { services, error };
+};
+
+export default useGetServices;
