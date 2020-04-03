e39592e8e3ee12def8aa44c7181740cb094dbaf7
Danni <dannimb@gmail.com>
2020-04-03 13:34:27 +0000

    added docs to Listextensions

--- Bakendi/ThjonustukerfiWebAPI/Extensions/ListExtensions.cs
+++ Bakendi/ThjonustukerfiWebAPI/Extensions/ListExtensions.cs
@@ -2,12 +2,17 @@ using System.Collections.Generic;
 
 namespace ThjonustukerfiWebAPI.Extensions
 {
+    /// <summary>Provides extensions to the List class</summary>
     public static class ListExtensions
     {
-        // Removes all duplicate items in other list
-        public static void RemoveExisting<T>(this IList<T> self, IEnumerable<T> items)
+        /// <summary>
+        ///     Removes all items in self that are the same as in the other list.
+        ///     
+        ///     Note that classes used in this method should have the all the Equals methods overrided to work.
+        /// </summary>
+        public static void RemoveExisting<T>(this IList<T> self, IEnumerable<T> other)
         {
-            foreach(var item in items)
+            foreach(var item in other)
             {
                 if(self.Contains(item)) { self.Remove(item); }
             }
