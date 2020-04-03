67b4c7d91927ca4551def2333b7e2c28d9c8a61c
Danni <dannimb@gmail.com>
2020-04-03 12:05:30 +0000

    removed unused using statements from webhost extension

--- Bakendi/ThjonustukerfiWebAPI/Extensions/Webhostextension.cs
+++ Bakendi/ThjonustukerfiWebAPI/Extensions/Webhostextension.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
