8c6a67268ce9cea25c9009384d32b08a9d756da9
Danni <dannimb@gmail.com>
2020-04-03 13:00:45 +0000

    removed unused using statement in program

--- Bakendi/ThjonustukerfiWebAPI/Program.cs
+++ Bakendi/ThjonustukerfiWebAPI/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using ThjonustukerfiWebAPI.Models;
 using ThjonustukerfiWebAPI.Extensions;
 
 namespace ThjonustukerfiWebAPI
