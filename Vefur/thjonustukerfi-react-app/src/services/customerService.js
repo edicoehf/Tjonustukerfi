a693c46cdb86a33a780323c6e851f753db173795
Megg <arnimegg@gmail.com>
2020-04-04 14:31:41 +0000

    backend call to get all customers

--- Vefur/thjonustukerfi-react-app/src/services/customerService.js
+++ Vefur/thjonustukerfi-react-app/src/services/customerService.js
@@ -2,6 +2,18 @@ import { handleErrors, handleData } from "./serviceHandlers";
 
 const endpoint = "http://localhost:5000/api/customers/";
 
+const getAllCustomers = () => {
+    return fetch(endpoint, {
+        method: "GET",
+        headers: {
+            crossDomain: true
+        }
+    })
+        .then(handleErrors)
+        .then(handleData)
+        .catch(error => Promise.reject(error));
+};
+
 const deleteCustomerById = id => {
     console.log(id);
 
@@ -54,6 +66,7 @@ const updateCustomer = customer => {
 };
 
 export default {
+    getAllCustomers,
     deleteCustomerById,
     createCustomer,
     getCustomerById,
