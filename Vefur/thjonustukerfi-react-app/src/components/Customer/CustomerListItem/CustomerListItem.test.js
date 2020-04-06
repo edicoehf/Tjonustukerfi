225f1292e60e212c6956141c4d61e7eb3cbea64a
Megg <arnimegg@gmail.com>
2020-04-06 09:05:40 +0000

    changed customerDTO so customerlist item could show email + test

--- Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerListItem/CustomerListItem.test.js
+++ Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerListItem/CustomerListItem.test.js
@@ -7,7 +7,7 @@ describe("<CustomerListItem />", () => {
     describe("CustomerListItem renders properly", () => {
         let wrapper;
         let customer;
-        const testCustomer = { id: "1", name: "arni" };
+        const testCustomer = { id: "1", name: "arni", email: "arni@arni.is" };
         let CustomerListItemComponent = (
             <CustomerListItem customer={testCustomer} />
         );
@@ -24,20 +24,33 @@ describe("<CustomerListItem />", () => {
             it("should not be null", () => {
                 expect(wrapper).not.toBeNull;
             });
+
             it("should have property that is not null", () => {
                 expect(customer).not.toBeNull;
             });
+
             it("should not have property with id 2", () => {
                 expect(customer.id).not.toEqual("2");
             });
+
             it("should have property with id 1", () => {
                 expect(customer.id).toEqual("1");
             });
+
             it("should not have property with name gunnar", () => {
                 expect(customer.name).not.toEqual("gunnar");
             });
+
             it("should have property with name arni", () => {
                 expect(customer.name).toEqual("arni");
+            });
+
+            it("should not have property with email arni@inra.is", () => {
+                expect(customer.email).not.toEqual("arni@inra.is");
+            });
+
+            it("should have property with email arni@arni.is", () => {
+                expect(customer.email).toEqual("arni@arni.is");
             });
         });
     });
