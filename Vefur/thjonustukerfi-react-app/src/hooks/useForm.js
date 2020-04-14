3497295407402b0e9d3bb70b5cd7fbb62dc0a0a2
ViktorSveins <viktorsveins@gmail.com>
2020-04-14 15:42:41 +0000

    useForm handlechangetext -> handlechange

--- Vefur/thjonustukerfi-react-app/src/hooks/useForm.js
+++ Vefur/thjonustukerfi-react-app/src/hooks/useForm.js
@@ -16,21 +16,21 @@ const useForm = (initialState, validate, submitHandler) => {
         }
     }, [errors, values, submitHandler, isSubmitting]);
 
-    const handleChangeText = e => {
+    const handleChange = (e) => {
         setValues({
             ...values,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         });
     };
 
-    const handleSubmit = e => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         const validationErrors = validate(values);
         setSubmitting(true);
         setErrors(validationErrors);
     };
 
-    return { handleSubmit, handleChangeText, values, errors };
+    return { handleSubmit, handleChange, values, errors };
 };
 
 export default useForm;
