ff5c04037ec3b53dc3fe24d0028789dd1ae3cb1e
ViktorSveins <viktorsveins@gmail.com>
2020-04-14 14:14:44 +0000

    categorieservice

--- Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/AddItems/AddItems.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/AddItems/AddItems.js
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+    Radio,
+    RadioGroup,
+    FormControlLabel,
+    FormControl,
+    FormLabel,
+} from "@material-ui/core";
 import useGetServices from "../../../../hooks/useGetServices";
 
 const { services, serviceError: error } = useGetServices();
@@ -6,7 +13,38 @@ const { services, serviceError: error } = useGetServices();
 const AddItems = () => {
     return (
         <div className="add-items">
-            <div></div>
+            <h3>Bæta við vöru</h3>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">Gender</FormLabel>
+                <RadioGroup
+                    aria-label="gender"
+                    name="gender1"
+                    value={value}
+                    onChange={handleChange}
+                >
+                    <FormControlLabel
+                        value="female"
+                        control={<Radio />}
+                        label="Female"
+                    />
+                    <FormControlLabel
+                        value="male"
+                        control={<Radio />}
+                        label="Male"
+                    />
+                    <FormControlLabel
+                        value="other"
+                        control={<Radio />}
+                        label="Other"
+                    />
+                    <FormControlLabel
+                        value="disabled"
+                        disabled
+                        control={<Radio />}
+                        label="(Disabled option)"
+                    />
+                </RadioGroup>
+            </FormControl>
         </div>
     );
 };
