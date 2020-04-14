748e2aefe2326c6e1c3bc79f4965a662502ee5eb
ViktorSveins <viktorsveins@gmail.com>
2020-04-14 15:20:11 +0000

    radiogroups for cat&serv and amount

--- Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/AddItems/AddItems.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/AddItems/AddItems.js
@@ -5,45 +5,63 @@ import {
     FormControlLabel,
     FormControl,
     FormLabel,
+    ButtonGroup,
+    Button,
 } from "@material-ui/core";
 import useGetServices from "../../../../hooks/useGetServices";
+import useGetCategories from "../../../../hooks/useGetCategories";
 
 const { services, serviceError: error } = useGetServices();
+const { categories, categoryError: error } = useGetCategories();
 
 const AddItems = () => {
     return (
         <div className="add-items">
             <h3>Bæta við vöru</h3>
             <FormControl component="fieldset">
-                <FormLabel component="legend">Gender</FormLabel>
+                <FormLabel component="legend">Tegund:</FormLabel>
                 <RadioGroup
-                    aria-label="gender"
-                    name="gender1"
+                    name="category"
                     value={value}
                     onChange={handleChange}
                 >
-                    <FormControlLabel
-                        value="female"
-                        control={<Radio />}
-                        label="Female"
-                    />
-                    <FormControlLabel
-                        value="male"
-                        control={<Radio />}
-                        label="Male"
-                    />
-                    <FormControlLabel
-                        value="other"
-                        control={<Radio />}
-                        label="Other"
-                    />
-                    <FormControlLabel
-                        value="disabled"
-                        disabled
-                        control={<Radio />}
-                        label="(Disabled option)"
-                    />
+                    {categories.map((cat) => (
+                        <FormControlLabel
+                            value={cat.id}
+                            control={<Radio />}
+                            label={cat.name}
+                        />
+                    ))}
                 </RadioGroup>
+            </FormControl>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">Þjónusta:</FormLabel>
+                <RadioGroup
+                    name="services"
+                    value={value}
+                    onChange={handleChange}
+                >
+                    {services.map((serv) => (
+                        <FormControlLabel
+                            value={serv.id}
+                            control={<Radio />}
+                            label={serv.name}
+                        />
+                    ))}
+                </RadioGroup>
+            </FormControl>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">Fjöldi:</FormLabel>
+                <ButtonGroup size="small">
+                    <Button
+                        disabled={counter < 1}
+                        onClick={this.handleDecrement}
+                    >
+                        -
+                    </Button>
+                    <Button disabled>{this.state.counter}</Button>
+                    <Button onClick={this.handleIncrement}>+</Button>
+                </ButtonGroup>
             </FormControl>
         </div>
     );
