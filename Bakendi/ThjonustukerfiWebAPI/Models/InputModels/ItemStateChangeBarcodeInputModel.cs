2998371b5f384ef7d88a7617b1d95bdaf5bb7345
Danni <dannimb@gmail.com>
2020-04-15 14:44:50 +0000

    added overrides to input models for comparison in tests

--- Bakendi/ThjonustukerfiWebAPI/Models/InputModels/ItemStateChangeBarcodeInputModel.cs
+++ Bakendi/ThjonustukerfiWebAPI/Models/InputModels/ItemStateChangeBarcodeInputModel.cs
@@ -4,5 +4,47 @@ namespace ThjonustukerfiWebAPI.Models.InputModels
     {
         public string Barcode { get; set; }
         public long StateChangeTo { get; set; }
+
+        //*     Overrides     *//
+        public static bool operator ==(ItemStateChangeBarcodeInputModel i1, ItemStateChangeBarcodeInputModel i2)
+        {
+            if(object.ReferenceEquals(i1, i2))
+            {
+                return true;
+            }
+            if(object.ReferenceEquals(i1, null) || object.ReferenceEquals(i2, null))
+            {
+                return false;
+            }
+
+            return i1.Barcode == i2.Barcode && i1.StateChangeTo == i2.StateChangeTo;
+        }
+
+        public static bool operator !=(ItemStateChangeBarcodeInputModel i1, ItemStateChangeBarcodeInputModel i2)
+        {
+            return !(i1 == i2);
+        }
+
+        public override int GetHashCode()
+        {
+            int bc = 0;
+            foreach (char c in Barcode)
+            {
+                bc += c.GetHashCode();
+            }
+            bc += StateChangeTo.GetHashCode();
+            
+            return bc;
+        }
+
+        public bool Equals(ItemStateChangeBarcodeInputModel other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ItemStateChangeBarcodeInputModel);
+        }
     }
 }
--- Bakendi/ThjonustukerfiWebAPI/Models/InputModels/ItemStateChangeInputModel.cs
+++ Bakendi/ThjonustukerfiWebAPI/Models/InputModels/ItemStateChangeInputModel.cs
@@ -5,5 +5,48 @@ namespace ThjonustukerfiWebAPI.Models.InputModels
         public long ItemId { get; set; }
         public string Barcode { get; set; }
         public long StateChangeTo { get; set; }
+
+        //*     Overrides     *//
+        public static bool operator ==(ItemStateChangeInputModel i1, ItemStateChangeInputModel i2)
+        {
+            if(object.ReferenceEquals(i1, i2))
+            {
+                return true;
+            }
+            if(object.ReferenceEquals(i1, null) || object.ReferenceEquals(i2, null))
+            {
+                return false;
+            }
+
+            return i1.Barcode == i2.Barcode && i1.StateChangeTo == i2.StateChangeTo && i1.ItemId == i2.ItemId;
+        }
+
+        public static bool operator !=(ItemStateChangeInputModel i1, ItemStateChangeInputModel i2)
+        {
+            return !(i1 == i2);
+        }
+
+        public override int GetHashCode()
+        {
+            int bc = 0;
+            foreach (char c in Barcode)
+            {
+                bc += c.GetHashCode();
+            }
+            bc += StateChangeTo.GetHashCode();
+            bc += ItemId.GetHashCode();
+            
+            return bc;
+        }
+
+        public bool Equals(ItemStateChangeInputModel other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ItemStateChangeInputModel);
+        }
     }
 }
