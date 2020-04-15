ecdc21524e007e8be543bf33718ebf13143e3c08
ViktorSveins <viktorsveins@gmail.com>
2020-04-15 14:36:47 +0000

    Viewitems

---
+++ Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/ViewItems/ViewItems.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Fab,
+} from "@material-ui/core";
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
+
+const ViewItems = ({ items, remove }) => {
+    return (
+        <div className="view-items">
+            <h3>Vörur</h3>
+            <TableContainer component={Paper}>
+                <Table stickyHeader aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Tegund</TableCell>
+                            <TableCell>Þjónusta</TableCell>
+                            <TableCell>Fjöldi</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {items.map((item, i) => (
+                            <TableRow key={i}>
+                                <TableCell>{item.categoryName}</TableCell>
+                                <TableCell>{item.serviceName}</TableCell>
+                                <TableCell>{item.amount}</TableCell>
+                                <TableCell align="right">
+                                    <Fab
+                                        onClick={() => remove(item)}
+                                        size="small"
+                                    >
+                                        <RemoveCircleOutlineIcon />
+                                    </Fab>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
+};
+
+export default ViewItems;
