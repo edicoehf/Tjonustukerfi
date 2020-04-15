385ed343ff59a949b79072f90ed2bd163e935e7d
ViktorSveins <viktorsveins@gmail.com>
2020-04-15 17:24:52 +0000

    open modal to select

--- Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/AddCustomer/AddCustomer.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/AddCustomer/AddCustomer.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import PickCustomerModal from "../PickCustomerModal/PickCustomerModal";
 
-const AddCustomer = () => {
-    return <div className="add-customer"></div>;
+const AddCustomer = ({ customer, addCustomer }) => {
+    const [modalOpen, setModalOpen] = React.useState(false);
+    const handleOpen = () => {
+        setModalOpen(true);
+    };
+    const handleClose = () => {
+        setModalOpen(false);
+    };
+
+    return (
+        <div className="add-customer">
+            <h3>Viðskiptavinur</h3>
+            {!customer ? (
+                <Button
+                    className="sbm-btn"
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    startIcon={<AddBoxIcon />}
+                    onClick={handleOpen}
+                >
+                    Velja viðskiptavin
+                </Button>
+            ) : (
+                <></>
+            )}
+            <PickCustomerModal open={modalOpen} handleClose={handleClose} />
+        </div>
+    );
 };
 
 export default AddCustomer;
