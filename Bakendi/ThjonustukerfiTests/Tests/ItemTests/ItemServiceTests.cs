84b3d669506a910ffe06ba59bda68da30002d8b7
Danni <dannimb@gmail.com>
2020-04-15 14:46:23 +0000

    new tests for changed functionality of changing item states in itemServiceTests

--- Bakendi/ThjonustukerfiTests/Tests/ItemTests/ItemServiceTests.cs
+++ Bakendi/ThjonustukerfiTests/Tests/ItemTests/ItemServiceTests.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ThjonustukerfiWebAPI.Mappings;
 using ThjonustukerfiWebAPI.Models.DTOs;
+using ThjonustukerfiWebAPI.Models.Exceptions;
+using ThjonustukerfiWebAPI.Models.InputModels;
 using ThjonustukerfiWebAPI.Repositories.Interfaces;
 using ThjonustukerfiWebAPI.Services.Implementations;
 using ThjonustukerfiWebAPI.Services.Interfaces;
@@ -82,6 +85,96 @@ namespace ThjonustukerfiTests.Tests.ItemTests
             //* Assert
             Assert.IsNotNull(retVal);
             Assert.IsInstanceOfType(retVal, typeof(ItemStateDTO));
+        }
+
+        [TestMethod]
+        public void ChangeItemStateBarcode_should_return_invalid_list()
+        {
+            string validBarcode = "983764892374";
+            string invalidbarcode = "-1";
+            //* Arrange
+            var input = new List<ItemStateChangeBarcodeInputModel>()
+            {
+                new ItemStateChangeBarcodeInputModel { Barcode = invalidbarcode, StateChangeTo = 1 },
+                new ItemStateChangeBarcodeInputModel { Barcode = validBarcode, StateChangeTo = 2 }
+            };
+            
+            var invalidInputs = new List<ItemStateChangeBarcodeInputModel>()
+            {
+                new ItemStateChangeBarcodeInputModel { Barcode = invalidbarcode, StateChangeTo = 1 }
+            };
+
+            // Mock Repo
+            _itemRepoMock.Setup(method => method.SearchItem(invalidbarcode)).Throws(new NotFoundException("Some message"));
+            _itemRepoMock.Setup(method => method.SearchItem(validBarcode)).Returns(1);
+            _itemRepoMock.Setup(method => method.ChangeItemStateById(It.IsAny<List<ItemStateChangeInputModel>>()))
+                .Returns(new List<ItemStateChangeInputModel>());
+
+            // create service
+            _itemService = new ItemService(_itemRepoMock.Object, _mapper);
+
+            //* Act
+            var retVal = _itemService.ChangeItemStateBarcode(input);
+
+            //* Assert
+            Assert.IsNotNull(retVal);
+            Assert.AreEqual(invalidInputs.Count, retVal.Count);
+            Assert.AreEqual(invalidInputs[0], retVal[0]);
+        }
+
+        [TestMethod]
+        public void ChangeItemStateBarcode_should_return_empty_list()
+        {
+            string validBarcode = "983764892374";
+            string validBarcode2 = "983764892375";
+            //* Arrange
+            var input = new List<ItemStateChangeBarcodeInputModel>()
+            {
+                new ItemStateChangeBarcodeInputModel { Barcode = validBarcode, StateChangeTo = 1 },
+                new ItemStateChangeBarcodeInputModel { Barcode = validBarcode2, StateChangeTo = 2 }
+            };
+
+            // Mock Repo
+            _itemRepoMock.Setup(method => method.SearchItem(validBarcode)).Returns(1);
+            _itemRepoMock.Setup(method => method.ChangeItemStateById(It.IsAny<List<ItemStateChangeInputModel>>()))
+                .Returns(new List<ItemStateChangeInputModel>());
+
+            // create service
+            _itemService = new ItemService(_itemRepoMock.Object, _mapper);
+
+            //* Act
+            var retVal = _itemService.ChangeItemStateBarcode(input);
+
+            //* Assert
+            Assert.IsNotNull(retVal);
+            Assert.AreEqual(0, retVal.Count);
+        }
+
+        [TestMethod]
+        public void ChangeItemStateById_should_return_empty_list()
+        {
+            long validId = 1;
+            long validId2 = 2;
+            //* Arrange
+            var input = new List<ItemStateChangeInputModel>()
+            {
+                new ItemStateChangeInputModel { ItemId = validId, StateChangeTo = 1 },
+                new ItemStateChangeInputModel { ItemId = validId2, StateChangeTo = 2 }
+            };
+
+            // Mock Repo
+            _itemRepoMock.Setup(method => method.ChangeItemStateById(It.IsAny<List<ItemStateChangeInputModel>>()))
+                .Returns(new List<ItemStateChangeInputModel>());
+
+            // create service
+            _itemService = new ItemService(_itemRepoMock.Object, _mapper);
+
+            //* Act
+            var retVal = _itemService.ChangeItemStateById(input);
+
+            //* Assert
+            Assert.IsNotNull(retVal);
+            Assert.AreEqual(0, retVal.Count);
         }
     }
 }
