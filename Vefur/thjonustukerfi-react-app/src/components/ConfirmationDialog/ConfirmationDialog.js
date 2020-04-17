0127938b22cf11c1635515e610f3054a17e2670c
ViktorSveins <viktorsveins@gmail.com>
2020-04-17 15:27:22 +0000

    general use confirmation popup

---
+++ Vefur/thjonustukerfi-react-app/src/components/ConfirmationDialog/ConfirmationDialog.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    Button,
+} from "@material-ui/core";
+
+const ConfirmationDialog = ({
+    title,
+    description,
+    handleAccept,
+    handleClose,
+    open,
+    declineText,
+    confirmText,
+}) => {
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            {title && (
+                <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+            )}
+            {description && (
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {description}
+                    </DialogContentText>
+                </DialogContent>
+            )}
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    {declineText ? declineText : "Til baka"}
+                </Button>
+                <Button onClick={handleAccept} color="primary" autoFocus>
+                    {confirmText ? confirmtext : "Staðfesta"}
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default ConfirmationDialog;
