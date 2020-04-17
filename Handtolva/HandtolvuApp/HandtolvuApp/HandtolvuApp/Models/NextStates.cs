99014e3ad0a6a0dfa618e6e2f8a1abd175857dcb
Danni <dannimb@gmail.com>
2020-04-17 11:52:36 +0000

    added get next states in xamarin app and a button to test it on the landing page

---
+++ Handtolva/HandtolvuApp/HandtolvuApp/HandtolvuApp/Models/NextStates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandtolvuApp.Models
+{
+    public class NextStates
+    {
+        public State CurrentState { get; set; }
+        public List<State> NextAvailableStates { get; set; }
+
+        //*     Overrides     *//
+
+        public override string ToString()
+        {
+            string print = $"Current State => {CurrentState}";
+
+            foreach(var state in NextAvailableStates)
+            {
+                print += $"\n{state}";
+            }
+
+            return print;
+        }
+    }
+}
