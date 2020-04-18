7bec0bcb0f0775b9a18e319771e30a95be7b8230
ViktorSveins <viktorsveins@gmail.com>
2020-04-18 20:29:09 +0000

    itemdetails component

---
+++ Vefur/thjonustukerfi-react-app/src/components/Item/ItemDetails/ItemDetails.js
@@ -0,0 +1,37 @@
+import React from "react";
+import useGetItemById from "../../../hooks/useGetItemById";
+import ItemStates from "../ItemStates/ItemStates";
+import { Link } from "react-router-dom";
+import "./ItemDetails.css";
+
+const ItemDetails = ({ id }) => {
+    const { item, error } = useGetItemById(id);
+
+    const itemProp = item ? [item] : [];
+
+    return (
+        <div className="item-details">
+            {!error ? (
+                <>
+                    <div className="item-title">Vara: {item.id}</div>
+                    <div className="item-category">Tegund: {item.category}</div>
+                    <div className="item-service">Þjónusta: {item.service}</div>
+                    <div className="item-currentstate">Staða: {item.state}</div>
+                    <div className="item-order">
+                        <Link to={`/customer/${item.orderId}`}>
+                            Pöntun: {item.orderId}
+                        </Link>
+                    </div>
+                    <div className="nextsteps">
+                        Næstu stöður:
+                        <ItemStates id={id} />
+                    </div>
+                </>
+            ) : (
+                <p className="error">Gat ekki sótt upplýsingar um vöru</p>
+            )}
+        </div>
+    );
+};
+
+export default ItemDetails;
