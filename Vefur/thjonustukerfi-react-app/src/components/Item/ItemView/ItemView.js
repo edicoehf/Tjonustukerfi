10156c0435cf9f4c01936d3ae66708409e1278b2
ViktorSveins <viktorsveins@gmail.com>
2020-04-19 17:04:51 +0000

    ItemView uses state to inform itemstate updates

--- Vefur/thjonustukerfi-react-app/src/components/Item/ItemView/ItemView.js
+++ Vefur/thjonustukerfi-react-app/src/components/Item/ItemView/ItemView.js
@@ -5,12 +5,25 @@ import "./ItemView.css";
 
 const ItemView = ({ match }) => {
     const id = match.params.id;
+    const [update, setUpdate] = React.useState(false);
+
+    const hasUpdated = () => {
+        setUpdate(true);
+    };
+
+    const receivedUpdate = () => {
+        setUpdate(false);
+    };
 
     return (
         <div className="item-view">
             <h1>Upplýsingar um vöru</h1>
-            <ItemDetails id={id} />
-            <StateSelection id={id} />
+            <ItemDetails
+                id={id}
+                updated={update}
+                receivedUpdate={receivedUpdate}
+            />
+            <StateSelection id={id} hasUpdated={hasUpdated} />
         </div>
     );
 };
