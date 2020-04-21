a9fe35dccae11f4f3ae41509befa3118ae6f78db
ViktorSveins <viktorsveins@gmail.com>
2020-04-21 15:38:41 +0000

    styling edit button

--- Vefur/thjonustukerfi-react-app/src/components/Item/Actions/EditItemAction/EditItemAction.js
+++ Vefur/thjonustukerfi-react-app/src/components/Item/Actions/EditItemAction/EditItemAction.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import "./EditItemAction.css";
 
 const EditItemAction = ({ id }) => {
     return (
         <div className="edit-item">
-            <Link to={`/edit-item/${id}`}>
-                <Button color="amber">Breyta</Button>
+            <Link to={`/update-item/${id}`}>
+                <Button className="edit-item-button" variant="contained">
+                    Breyta
+                </Button>
             </Link>
         </div>
     );
