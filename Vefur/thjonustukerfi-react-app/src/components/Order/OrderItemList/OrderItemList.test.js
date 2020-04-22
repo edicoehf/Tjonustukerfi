ce2a7fe22906e4d5f1afbd4a791caa78ece3784e
ViktorSveins <viktorsveins@gmail.com>
2020-04-22 11:45:22 +0000

    fix order test, rows->columns

--- Vefur/thjonustukerfi-react-app/src/components/Order/OrderItemList/OrderItemList.test.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/OrderItemList/OrderItemList.test.js
@@ -30,9 +30,9 @@ describe("<OrderItemList />", () => {
         wrapper = mount(shallow(<OrderItemList items={testProps} />).get(0));
     });
 
-    it("Should have 3 rows", () => {
+    it("Should have 4 columns", () => {
         expect(
             wrapper.find("tr.order-item").at(0).instance().children.length
-        ).toBe(3);
+        ).toBe(4);
     });
 });
