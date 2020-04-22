ad81640057cd7b9dd5cad7437ad35d678cd600a7
ViktorSveins <viktorsveins@gmail.com>
2020-04-22 11:26:52 +0000

    styling

--- Vefur/thjonustukerfi-react-app/src/App.js
+++ Vefur/thjonustukerfi-react-app/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import NavigationBar from "./components/Header/NavigationBar/NavigationBar";
-import LandingPage from "./components/LandingPage/LandingPage";
+// import LandingPage from "./components/LandingPage/LandingPage";
 import CreateCustomer from "./components/Customer/CreateCustomer/CreateCustomer";
 import CustomerView from "./components/Customer/CustomerView/CustomerView";
 import UpdateCustomer from "./components/Customer/UpdateCustomer/UpdateCustomer";
--- Vefur/thjonustukerfi-react-app/src/components/Order/OrderItem/OrderItem.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/OrderItem/OrderItem.js
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 const OrderItem = ({ item }) => {
     const { id, category, service, barcode } = item;
     return (
-        <Link to={`/item/${id}`}>
-            <tr className="order-item">
-                <td className="order-item-category">{category}</td>
-                <td className="order-item-service">{service}</td>
-                <td className="order-item-barcode">{barcode}</td>
-            </tr>
-        </Link>
+        <tr className="order-item">
+            <td className="order-item-id">
+                <Link to={`/item/${id}`}>{id}</Link>
+            </td>
+            <td className="order-item-category">{category}</td>
+            <td className="order-item-service">{service}</td>
+            <td className="order-item-barcode">{barcode}</td>
+        </tr>
     );
 };
 
