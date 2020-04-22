49e2d83e9b0121b4b9791d862ade6c3dd9d40cc3
Danni <dannimb@gmail.com>
2020-04-22 13:51:10 +0000

    equals override in iteminputmodel

--- Bakendi/ThjonustukerfiWebAPI/Models/InputModels/ItemInputModel.cs
+++ Bakendi/ThjonustukerfiWebAPI/Models/InputModels/ItemInputModel.cs
@@ -1,9 +1,53 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ThjonustukerfiWebAPI.Models.InputModels
 {
     /// <summary>Input model to add new Item to the database.</summary>
     public class ItemInputModel
     {
+        [Required]
         public long? CategoryId { get; set; }
+        [Required]
         public long? ServiceId { get; set; }
+
+        //*     Overrides     *//
+        public static bool operator ==(ItemInputModel i1, ItemInputModel i2)
+        {
+            if(object.ReferenceEquals(i1, i2))
+            {
+                return true;
+            }
+            if(object.ReferenceEquals(i1, null) || object.ReferenceEquals(i2, null))
+            {
+                return false;
+            }
+
+            return i1.CategoryId == i2.CategoryId && i1.ServiceId == i2.ServiceId;
+        }
+
+        public static bool operator !=(ItemInputModel i1, ItemInputModel i2)
+        {
+            return !(i1 == i2);
+        }
+
+        public override int GetHashCode()
+        {
+            int bc = 0;
+
+            bc += CategoryId.GetHashCode();
+            bc += ServiceId.GetHashCode();
+            
+            return bc;
+        }
+
+        public bool Equals(ItemInputModel other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ItemInputModel);
+        }
     }
 }
