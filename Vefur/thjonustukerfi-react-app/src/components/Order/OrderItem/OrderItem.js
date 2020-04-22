00996a2c7a3732062b7a5c45cefc7149de3ee338
ViktorSveins <viktorsveins@gmail.com>
2020-04-22 10:44:31 +0000

    link to item details in order details

--- Vefur/thjonustukerfi-react-app/src/components/Order/OrderItem/OrderItem.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/OrderItem/OrderItem.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { itemType } from "../../../types/index";
+import { Link } from "react-router-dom";
 
 const OrderItem = ({ item }) => {
-    const { category, service, barcode } = item;
+    const { id, category, service, barcode } = item;
     return (
-        <tr className="order-item">
-            <td className="order-item-category">{category}</td>
-            <td className="order-item-service">{service}</td>
-            <td className="order-item-barcode">{barcode}</td>
-        </tr>
+        <Link to={`/item/${id}`}>
+            <tr className="order-item">
+                <td className="order-item-category">{category}</td>
+                <td className="order-item-service">{service}</td>
+                <td className="order-item-barcode">{barcode}</td>
+            </tr>
+        </Link>
     );
 };
 
