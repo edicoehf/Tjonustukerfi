8c4f783d8d6cd727565ee0a3d91b4d66d8e89607
ViktorSveins <viktorsveins@gmail.com>
2020-04-23 16:06:35 +0000

    createorderview now uses orderform

--- Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/CreateOrderView/CreateOrderView.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/CreateOrderView/CreateOrderView.js
@@ -1,85 +1,47 @@
 import React from "react";
-import AddItems from "../AddItems/AddItems";
-import ViewItems from "../ViewItems/ViewItems";
-import useGetServices from "../../../../hooks/useGetServices";
-import useGetCategories from "../../../../hooks/useGetCategories";
-import AddCustomer from "../AddCustomer/AddCustomer";
-import "./CreateOrderView.css";
 import CreateOrderActions from "../OrderActions/CreateOrderActions";
 import useCreateOrder from "../../../../hooks/useCreateOrder";
 import orderValidate from "../OrderValidate/OrderValidate";
+import useOrderForm from "../../../../hooks/useOrderForm";
+import "./CreateOrderView.css";
+import OrderForm from "../OrderForm/OrderForm";
+
+const initialState = {
+    customer: null,
+    items: [],
+};
 
 const CreateOrderView = () => {
-    const [items, setItems] = React.useState([]);
-    const [customer, setCustomer] = React.useState(null);
-    const [errors, setErrors] = React.useState({});
-    const { services } = useGetServices();
-    const { categories } = useGetCategories();
-
     const { error: sendError, handleCreate, isProcessing } = useCreateOrder();
-
-    const addItems = (newItem, cb) => {
-        const ids = items.map((item) => item.id);
-        newItem.id = ids.reduce((acc, curr) => Math.max(acc, curr), 0) + 1;
-        setItems([...items, newItem]);
-        cb();
-    };
-
-    const removeItem = (itemToRemove) => {
-        setItems(items.filter((item) => itemToRemove.id !== item.id));
-    };
-
-    const addCustomer = (customer) => {
-        setCustomer(customer);
-    };
+    const {
+        addItems,
+        removeItem,
+        addCustomer,
+        handleSubmit,
+        resetFields,
+        items,
+        customer,
+        errors,
+    } = useOrderForm(initialState, orderValidate, handleCreate);
 
     const createOrder = () => {
-        const err = orderValidate(customer, items);
-        setErrors(err);
-        if (!isProcessing && Object.keys(err).length === 0) {
-            let order = {
-                customerId: null,
-                items: [],
-            };
-            order.customerId = customer.id;
-            items.forEach((item) => {
-                for (var i = 0; i < item.amount; i++) {
-                    console.log(item);
-                    order.items.push({
-                        categoryId: item.category,
-                        serviceId: item.service,
-                    });
-                }
-            });
-            handleCreate(order);
-            if (!sendError) {
-                setCustomer(null);
-                setItems([]);
-            }
+        if (!isProcessing) {
+            handleSubmit();
         }
     };
 
-    const cancelOrder = () => {
-        setCustomer(null);
-        setItems([]);
-    };
     return (
-        <div className="create-order-view-container">
-            <div className="create-order-view">
-                <AddItems
-                    addItems={addItems}
-                    categories={categories}
-                    services={services}
-                />
-                <ViewItems items={items} remove={removeItem} />
-                <AddCustomer customer={customer} addCustomer={addCustomer} />
-            </div>
+        <div className="create-order-view">
+            <OrderForm
+                values={{ items, customer }}
+                functions={{ addItems, removeItem, addCustomer }}
+            />
             {sendError && <p className="error">Gat ekki sent inn pöntun</p>}
-            {errors.category && <p className="error">{errors.category}</p>}
-            {errors.service && <p className="error">{errors.service}</p>}
+            {errors.customer && <p className="error">{errors.customer}</p>}
+            {errors.items && <p className="error">{errors.items}</p>}
             <CreateOrderActions
                 createOrder={createOrder}
-                cancelOrder={cancelOrder}
+                cancelOrder={resetFields}
             />
         </div>
     );
