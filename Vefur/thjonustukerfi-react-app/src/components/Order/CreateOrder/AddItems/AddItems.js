225d8c9d4d0034efe325166ccddfdbb10e524abc
ViktorSveins <viktorsveins@gmail.com>
2020-04-23 11:42:11 +0000

    moved part of addItem logic into addItem component

--- Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/AddItems/AddItems.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/AddItems/AddItems.js
@@ -8,13 +8,28 @@ import {
 } from "../../../../types/index";
 
 const AddItems = ({ addItems, categories, services }) => {
+    const handleAdd = (newItem, cb) => {
+        newItem.serviceName =
+            services[
+                services.findIndex((s) => s.id.toString() === newItem.service)
+            ].name;
+        newItem.categoryName =
+            categories[
+                categories.findIndex(
+                    (c) => c.id.toString() === newItem.category
+                )
+            ].name;
+
+        addItems(newItem, cb);
+    };
+
     return (
         <div className="add-items">
             <h3>Bæta við vöru</h3>
             <ItemForm
                 categories={categories}
                 services={services}
-                submitHandler={addItems}
+                submitHandler={handleAdd}
             />
         </div>
     );
