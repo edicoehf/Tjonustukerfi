c20a4df441feb08b7a80b612b7ff6f47ade5284b
ViktorSveins <viktorsveins@gmail.com>
2020-04-23 16:05:53 +0000

    working orderform component

--- Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/OrderForm/OrderForm.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/OrderForm/OrderForm.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { orderFormValuesType, orderFormFunctionsType } from "../../../../types";
+import useGetCategories from "../../../../hooks/useGetCategories";
+import useGetServices from "../../../../hooks/useGetServices";
+import AddItems from "../AddItems/AddItems";
+import ViewItems from "../ViewItems/ViewItems";
+import AddCustomer from "../AddCustomer/AddCustomer";
+import "./OrderForm.css";
 
-const OrderForm = () => {
+const OrderForm = ({ values, functions }) => {
+    const { items, customer } = values;
+    const { addItems, removeItem, addCustomer } = functions;
     const { services } = useGetServices();
     const { categories } = useGetCategories();
 
@@ -15,6 +24,11 @@ const OrderForm = () => {
             <AddCustomer customer={customer} addCustomer={addCustomer} />
         </div>
     );
+};
+
+OrderForm.propTypes = {
+    values: orderFormValuesType,
+    functions: orderFormFunctionsType,
 };
 
 export default OrderForm;
