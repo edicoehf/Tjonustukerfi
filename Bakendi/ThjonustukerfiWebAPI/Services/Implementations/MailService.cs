754a3ba76f288873cc9596799eeb4395f10440e2
Danni <dannimb@gmail.com>
2020-04-24 09:11:37 +0000

    mailservice setup with mailgun

---
+++ Bakendi/ThjonustukerfiWebAPI/Services/Implementations/MailService.cs
@@ -0,0 +1,25 @@
+using System;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace ThjonustukerfiWebAPI.Services.Implementations
+{
+    public static class MailService
+    {
+        public static IRestResponse Sendmail (string emailTo, string subject, string body)
+        {
+            RestClient client = new RestClient ();
+            client.BaseUrl = new Uri ("https://api.mailgun.net/v3/sandboxfd3dcd967775490d82138a8f336fb6b2.mailgun.org/messages");
+            client.Authenticator =
+                new HttpBasicAuthenticator ("api",
+                    "97ee06aae75d080a18d280122fcadc89-816b23ef-ebe3f667");
+            RestRequest request = new RestRequest ();
+            request.AddParameter ("from", "Mailgun Sandbox <postmaster@sandboxfd3dcd967775490d82138a8f336fb6b2.mailgun.org>");
+            request.AddParameter ("to", "dannimb@gmail.com");
+            request.AddParameter ("subject", subject);
+            request.AddParameter ("text", body);
+            request.Method = Method.POST;
+            return client.Execute (request);
+        }
+    }
+}
