cf883ba6447be0ebdb7790bb4abd81b4ca688c66
ViktorSveins <viktorsveins@gmail.com>
2020-04-25 15:14:24 +0000

    added checkout to order actions

--- Vefur/thjonustukerfi-react-app/src/components/Order/Actions/OrderActions/OrderActions.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/Actions/OrderActions/OrderActions.js
@@ -3,12 +3,14 @@ import DeleteOrderAction from "../DeleteOrderAction/DeleteOrderAction";
 import { idType } from "../../../../types";
 import UpdateOrderAction from "../UpdateOrderAction/UpdateOrderAction";
 import "./OrderActions.css";
+import CheckoutOrderAction from "../CheckoutOrderAction/CheckoutOrderAction";
 
 const OrderActions = ({ id }) => {
     return (
         <div className="order-actions">
             <DeleteOrderAction id={id} />
             <UpdateOrderAction id={id} />
+            <CheckoutOrderAction id={id} />
         </div>
     );
 };
