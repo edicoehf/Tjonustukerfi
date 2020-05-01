7157144060c5d47dc619921c2ced4e6fc7a87de9
ViktorSveins <viktorsveins@gmail.com>
2020-05-01 21:43:06 +0000

    redid customerdetails

--- Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerProperty/CustomerProperty.js
+++ Vefur/thjonustukerfi-react-app/src/components/Customer/CustomerProperty/CustomerProperty.js
@@ -1,15 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./CustomerProperty.css";
 import { TableRow, TableCell } from "@material-ui/core";
 
 const CustomerProperty = ({ title, name, value }) => {
     return (
         <>
             {value ? (
-                <TableRow key={name} title={name}>
-                    <TableCell>{title}:</TableCell>
-                    <TableCell>{value}</TableCell>
+                <TableRow
+                    key={name}
+                    title={name}
+                    className="customer-details-row"
+                >
+                    <TableCell className="customer-details-title-cell">
+                        {title}:
+                    </TableCell>
+                    <TableCell className="customer-details-content-cell">
+                        {value}
+                    </TableCell>
                 </TableRow>
             ) : (
                 <></>
