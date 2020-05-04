e56fe1dabb2f247a5e2686bc48b0572e125fefe0
Megg <arnimegg@gmail.com>
2020-05-04 22:25:28 +0000

    fixed buttons for printing

--- Vefur/thjonustukerfi-react-app/src/components/Item/Actions/ItemActions/ItemActions.js
+++ Vefur/thjonustukerfi-react-app/src/components/Item/Actions/ItemActions/ItemActions.js
@@ -2,14 +2,12 @@ import React from "react";
 import EditItemAction from "../EditItemAction/EditItemAction";
 import "./ItemActions.css";
 import DeleteItemAction from "../DeleteItemAction/DeleteItemAction";
-import PrintItemAction from "../PrintItemAction/PrintItemAction";
 
-const ItemActions = ({ id, handlePrint }) => {
+const ItemActions = ({ id }) => {
     return (
         <div className="item-actions">
             <DeleteItemAction id={id} />
             <EditItemAction id={id} />
-            <PrintItemAction id={id} handlePrint={handlePrint} />
         </div>
     );
 };
