60458ccc9c67afd80ffeeff7d1b1787b192d598e
Danni <dannimb@gmail.com>
2020-05-04 11:38:02 +0000

    environment variables file added and a class that handles getting them, the files are ignored by git

---
+++ Bakendi/ThjonustukerfiWebAPI/EnvironmentVariables/EnvironmentFileManager.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace ThjonustukerfiWebAPI.EnvironmentVariables
+{
+    public static class EnvironmentFileManager
+    {
+        public static Dictionary<string, string> LoadEvironmentFile()
+        {
+            string envFile = "EnvironmentVariables\\.env";
+            var dic = new Dictionary<string, string>();
+
+            if (File.Exists(envFile))
+            {
+                var envData = File.ReadAllLines(envFile);
+                for (var i = 0; i < envData.Length; i++)
+                {
+                    var setting = envData[i];
+                    var sidx = setting.IndexOf("=");
+                    if (sidx >= 0)
+                    {
+                        var skey = setting.Substring(0, sidx);
+                        var svalue = setting.Substring(sidx+1);
+                        if (!dic.ContainsKey(skey))
+                        {
+                            dic.Add(skey, svalue);
+                        }
+                    }
+                }
+            }
+
+            return dic;
+        }
+    }
+}
