5d1fa0e798e6b3a5b2f2807a093e06be07fee05d
ViktorSveins <viktorsveins@gmail.com>
2020-05-05 19:38:22 +0000

    added progressc to item controlby childrens loadCB

--- Vefur/thjonustukerfi-react-app/src/components/Item/ItemView/ItemView.js
+++ Vefur/thjonustukerfi-react-app/src/components/Item/ItemView/ItemView.js
@@ -4,11 +4,16 @@ import StateSelection from "../StateSelection/StateSelection";
 import ItemStates from "../ItemStates/ItemStates";
 import ItemActions from "../Actions/ItemActions/ItemActions";
 import "./ItemView.css";
+import ProgressComponent from "../../Feedback/ProgressComponent/ProgressComponent";
 
 const ItemView = ({ match }) => {
     const id = match.params.id;
     const [detailsUpdate, setDetailsUpdate] = React.useState(false);
     const [statesUpdate, setStatesUpdate] = React.useState(false);
+    const [detailsLoading, setDetailsLoading] = React.useState(false);
+    const [nextStatesLoading, setNextStatesLoading] = React.useState(false);
+    const [prevStatesLoading, setPrevStatesLoading] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState(true);
 
     const hasUpdated = () => {
         setDetailsUpdate(true);
@@ -23,19 +28,34 @@ const ItemView = ({ match }) => {
         setStatesUpdate(false);
     };
 
+    React.useEffect(() => {
+        if (!detailsLoading && !nextStatesLoading && !prevStatesLoading) {
+            setIsLoading(false);
+        } else {
+            setIsLoading(true);
+        }
+    }, [detailsLoading, nextStatesLoading, prevStatesLoading]);
+
     return (
         <div className="item-view">
             <h1>Upplýsingar um vöru</h1>
+            <ProgressComponent isLoading={isLoading} />
             <ItemDetails
                 id={id}
                 updated={detailsUpdate}
                 receivedUpdate={detailsReceivedUpdate}
+                componentLoading={setDetailsLoading}
             />
-            <StateSelection id={id} hasUpdated={hasUpdated} />
+            <StateSelection
+                id={id}
+                hasUpdated={hasUpdated}
+                componentLoading={setNextStatesLoading}
+            />
             <ItemStates
                 id={id}
                 updated={statesUpdate}
                 receivedUpdate={statesReceivedUpdate}
+                componentLoading={setPrevStatesLoading}
             />
             <ItemActions id={id} />
         </div>
