148f77529cba0fdcbe7d0f30e36d27722f8627f4
ViktorSveins <viktorsveins@gmail.com>
2020-05-05 19:39:14 +0000

    added progressbutton to checkout

--- Vefur/thjonustukerfi-react-app/src/components/Order/Actions/CheckoutOrderAction/CheckoutOrderAction.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/Actions/CheckoutOrderAction/CheckoutOrderAction.js
@@ -3,7 +3,8 @@ import { Button } from "@material-ui/core";
 import CheckIcon from "@material-ui/icons/Check";
 import "./CheckoutOrderAction.css";
 import useCheckoutOrderById from "../../../../hooks/useCheckoutOrderById";
-import ConfirmationDialog from "../../../ConfirmationDialog/ConfirmationDialog";
+import ConfirmationDialog from "../../../Feedback/ConfirmationDialog/ConfirmationDialog";
+import ProgressButton from "../../../Feedback/ProgressButton/ProgressButton";
 
 const CheckoutOrderAction = ({ id, hasUpdated }) => {
     const { error, handleCheckout, isCheckingOut } = useCheckoutOrderById(
@@ -29,17 +30,19 @@ const CheckoutOrderAction = ({ id, hasUpdated }) => {
 
     return (
         <div className="checkout-order">
-            <Button
-                className="checkout-order-button"
-                size="medium"
-                color="primary"
-                variant="contained"
-                disabled={isCheckingOut}
-                onClick={handleOpen}
-            >
-                <CheckIcon className="checkout-icon" size="small" />
-                <b>Skrá sótt</b>
-            </Button>
+            <ProgressButton isLoading={isCheckingOut}>
+                <Button
+                    className="checkout-order-button"
+                    size="medium"
+                    color="primary"
+                    variant="contained"
+                    disabled={isCheckingOut}
+                    onClick={handleOpen}
+                >
+                    <CheckIcon className="checkout-icon" size="small" />
+                    <b>Skrá sótt</b>
+                </Button>
+            </ProgressButton>
             <ConfirmationDialog
                 title="Skrá pöntun sótta"
                 description="Staðfestu að skrá eigi pöntun sótta"
