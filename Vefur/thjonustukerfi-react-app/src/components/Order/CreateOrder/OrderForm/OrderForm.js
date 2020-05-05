6715a951cbbbbb7f4a7b0845156a29be25f50be0
ViktorSveins <viktorsveins@gmail.com>
2020-05-05 19:43:19 +0000

    added progress component to orderform

--- Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/OrderForm/OrderForm.js
+++ Vefur/thjonustukerfi-react-app/src/components/Order/CreateOrder/OrderForm/OrderForm.js
@@ -6,22 +6,46 @@ import AddItems from "../AddItems/AddItems";
 import ViewItems from "../ViewItems/ViewItems";
 import AddCustomer from "../AddCustomer/AddCustomer";
 import "./OrderForm.css";
+import ProgressComponent from "../../../Feedback/ProgressComponent/ProgressComponent";
 
 const OrderForm = ({ values, functions }) => {
     const { items, customer } = values;
     const { addItems, removeItem, addCustomer } = functions;
-    const { services } = useGetServices();
-    const { categories } = useGetCategories();
+    const {
+        services,
+        error: servicesError,
+        isLoading: servicesLoading,
+    } = useGetServices();
+    const {
+        categories,
+        error: categoriesError,
+        isLoading: categoriesLoading,
+    } = useGetCategories();
 
     return (
         <div className="order-form">
-            <AddItems
-                addItems={addItems}
-                categories={categories}
-                services={services}
-            />
-            <ViewItems items={items} remove={removeItem} />
-            <AddCustomer customer={customer} addCustomer={addCustomer} />
+            {servicesLoading || categoriesLoading ? (
+                <ProgressComponent
+                    isLoading={servicesLoading || categoriesLoading}
+                />
+            ) : !servicesError && !categoriesError ? (
+                <>
+                    <AddItems
+                        addItems={addItems}
+                        categories={categories}
+                        services={services}
+                    />
+                    <ViewItems items={items} remove={removeItem} />
+                    <AddCustomer
+                        customer={customer}
+                        addCustomer={addCustomer}
+                    />
+                </>
+            ) : (
+                <p className="error">
+                    Gat ekki sótt þær vörur og þjónustur sem í boði eru
+                </p>
+            )}
         </div>
     );
 };
