7ea09b2badedb64ae5f445fb8de069f2e1f86a5f
ViktorSveins <viktorsveins@gmail.com>
2020-05-06 11:37:10 +0000

    switch button position in forcedelete, consistency

--- Vefur/thjonustukerfi-react-app/src/components/Customer/Actions/ForceDeleteCustomerAction/ForceDeleteCustomerAction.js
+++ Vefur/thjonustukerfi-react-app/src/components/Customer/Actions/ForceDeleteCustomerAction/ForceDeleteCustomerAction.js
@@ -23,19 +23,19 @@ const ForceDeleteCustomerAction = ({ open, handleDelete, handleClose }) => {
                 <div className="modal-buttons">
                     <Button
                         variant="outlined"
-                        color="secondary"
-                        onClick={handleDelete}
-                    >
-                        <DeleteIcon />
-                        <h6 className="modal-button-text">Eyða</h6>
-                    </Button>
-                    <Button
-                        variant="outlined"
                         color="primary"
                         onClick={handleClose}
                     >
                         <CancelIcon />
                         <h6 className="modal-button-text">Hætta við</h6>
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="secondary"
+                        onClick={handleDelete}
+                    >
+                        <DeleteIcon />
+                        <h6 className="modal-button-text">Eyða</h6>
                     </Button>
                 </div>
             </div>
