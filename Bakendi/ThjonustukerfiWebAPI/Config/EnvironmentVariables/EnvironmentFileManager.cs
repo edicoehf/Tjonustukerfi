bf5eac452802e486da8e0aa1b540f04662b5cb5a
Danni <dannimb@gmail.com>
2020-05-11 14:44:42 +0000

    setting env for mail service in docker compose for web api

--- Bakendi/ThjonustukerfiWebAPI/Config/EnvironmentVariables/EnvironmentFileManager.cs
+++ Bakendi/ThjonustukerfiWebAPI/Config/EnvironmentVariables/EnvironmentFileManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -10,23 +11,46 @@ namespace ThjonustukerfiWebAPI.Config.EnvironmentVariables
         public static Dictionary<string, string> LoadEvironmentFile()
         {
             string envFile = "Config/EnvironmentVariables/.env";
+
+            if (File.Exists(envFile)) { return ParseEnvFile(envFile); }
+            else
+            {
+                string SMTP_USERNAME = Environment.GetEnvironmentVariable("SMTP_USERNAME");
+                string SMTP_PASSWORD = Environment.GetEnvironmentVariable("SMTP_PASSWORD");
+                string SMTP_SERVER = Environment.GetEnvironmentVariable("SMTP_SERVER");
+                string SMTP_PORT = Environment.GetEnvironmentVariable("SMTP_PORT");
+
+                var path = "Config/EnvironmentVariables";
+
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, ".env")))
+                {
+                    outputFile.WriteLine($"SMTP_USERNAME={SMTP_USERNAME}");
+                    outputFile.WriteLine($"SMTP_PASSWORD={SMTP_PASSWORD}");
+                    outputFile.WriteLine($"SMTP_SERVER={SMTP_SERVER}");
+                    outputFile.WriteLine($"SMTP_PORT={SMTP_PORT}");
+                }
+
+                if (File.Exists(envFile)) { return ParseEnvFile(envFile); }
+                else { return new Dictionary<string, string>(); }
+            }
+        }
+
+        private static Dictionary<string, string> ParseEnvFile(string envFile)
+        {
             var dic = new Dictionary<string, string>();
 
-            if (File.Exists(envFile))
+            var envData = File.ReadAllLines(envFile);
+            for (var i = 0; i < envData.Length; i++)
             {
-                var envData = File.ReadAllLines(envFile);
-                for (var i = 0; i < envData.Length; i++)
+                var setting = envData[i];
+                var sidx = setting.IndexOf("=");
+                if (sidx >= 0)
                 {
-                    var setting = envData[i];
-                    var sidx = setting.IndexOf("=");
-                    if (sidx >= 0)
+                    var skey = setting.Substring(0, sidx);
+                    var svalue = setting.Substring(sidx+1);
+                    if (!dic.ContainsKey(skey))
                     {
-                        var skey = setting.Substring(0, sidx);
-                        var svalue = setting.Substring(sidx+1);
-                        if (!dic.ContainsKey(skey))
-                        {
-                            dic.Add(skey, svalue);
-                        }
+                        dic.Add(skey, svalue);
                     }
                 }
             }
