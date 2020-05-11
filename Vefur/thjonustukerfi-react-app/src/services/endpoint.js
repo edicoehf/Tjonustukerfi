49f8f3ad2b111fd7a496fad4fd3ffedae098ceb7
Danni <dannimb@gmail.com>
2020-05-11 11:39:14 +0000

    setup env files and script to copy window values/variables to public in react app

--- Vefur/thjonustukerfi-react-app/src/services/endpoint.js
+++ Vefur/thjonustukerfi-react-app/src/services/endpoint.js
@@ -1,5 +1,5 @@
-const url = window._env_.API_URL || "http://localhost";
-const port = window._env_.API_PORT || "5000";
+const url = window._env_.API_URL;
+const port = window._env_.API_PORT;
 const endpoint = `${url}:${port}`;
 
 module.exports = endpoint;
