7a998f0f9b32cfa29a51bac9c51b4ce2930e02d1
Danni <dannimb@gmail.com>
2020-05-11 13:50:51 +0000

    fixed tests for new env changes

--- Vefur/thjonustukerfi-react-app/src/services/endpoint.js
+++ Vefur/thjonustukerfi-react-app/src/services/endpoint.js
@@ -1,5 +1,13 @@
-const url = window._env_.API_URL ;
-const port = window._env_.API_PORT;
+let envUrl = null;
+let envPort = null;
+
+if(window._env_) {
+    envUrl = window._env_.API_URL;
+    envPort = window._env_.API_PORT;
+}
+
+const url = envUrl || "http:localhost";
+const port = envPort || 5000;
 const endpoint = `${url}:${port}`;
 
 module.exports = endpoint;
